1146598eb0e7591f727d4c560ec1257bd36fd2e1
Sumanta Kundu <sumantasam1990@gmail.com>
2023-01-24 22:36:07 +0530

    register, login, logout, portfolio pages done.

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -13,6 +13,7 @@
     </head>
     <body class="antialiased bg-light">
 
+    @auth()
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-2" style="margin: 0; padding: 0;">
@@ -24,7 +25,21 @@
 
                     <ul class="mt-5">
                         <li><a href=""><i class="fa-solid fa-heart"></i> &nbsp; Contest</a> </li>
+
                         <li class="{{ (request()->is('model/portfolio')) ? 'active' : '' }}"><a href=""><i class="fa-solid fa-camera-retro"></i> &nbsp; Portfolio</a> </li>
+
+{{--                        <div class="dropdown">--}}
+{{--                            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">--}}
+{{--                                Dropdown link--}}
+{{--                            </a>--}}
+
+{{--                            <ul class="dropdown-menu">--}}
+{{--                                <li><a class="dropdown-item" href="#">Action</a></li>--}}
+{{--                                <li><a class="dropdown-item" href="#">Another action</a></li>--}}
+{{--                                <li><a class="dropdown-item" href="#">Something else here</a></li>--}}
+{{--                            </ul>--}}
+{{--                        </div>--}}
+
                         <li><a href=""><i class="fa-solid fa-person-circle-question"></i> &nbsp; Help</a> </li>
                         <li><a href=""><i class="fa-solid fa-credit-card"></i> &nbsp; Subscription</a> </li>
                         @if(Auth::check())
@@ -33,7 +48,9 @@
                             <li class="active"><a href="{{route('register')}}"><i class="fa-solid fa-user-plus"></i> &nbsp; Register</a> </li>
                             <li><a href="{{route('login')}}"><i class="fa-solid fa-right-to-bracket"></i> &nbsp; Sign In</a> </li>
                         @endif
+
                     </ul>
+
                 </div>
             </div>
 
@@ -47,6 +64,7 @@
             </div>
         </div>
     </div>
+    @endauth
 
     </body>
 </html>
